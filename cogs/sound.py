d8d27d003180bdbf0cf8fea7e023eb228c8dba9d
CRJL1001 <constantlescutprive2@gmail.com>
2026-09-20 13:57:37 +0200

    Sound toggle methode OK

--- cogs/sound.py
+++ cogs/sound.py
@@ -8,6 +8,7 @@ from config import SOUNDS, GUILD_ID, SOUND_CHANNEL_ID, VOCAL_SOUND_DELAY_INTERVA
 class Sounds(commands.Cog):
     def __init__(self, bot: commands.Bot ):
         self.bot = bot
+        self.sounds_enabled=True
 
     async def jouer_son(self):
         guild = self.bot.get_guild(GUILD_ID)
@@ -39,20 +40,48 @@ class Sounds(commands.Cog):
 
         await voice_client.disconnect()
 
-    @commands.Cog.listener()
-    async def on_ready(self):
-        self.bot.loop.create_task(self.boucle_aleatoire())
-
     async def boucle_aleatoire(self):
         await self.bot.wait_until_ready()
         while not self.bot.is_closed():
             delai = random.randint(VOCAL_SOUND_DELAY_INTERVAL[0], VOCAL_SOUND_DELAY_INTERVAL[1])
+
+            if not self.sounds_enabled:
+                print("Sons désactivés")
+                await asyncio.sleep(30)
+                continue
+
             print(f'prochain son dans {delai} secondes...')
             await asyncio.sleep(delai)
             try:
                 await self.jouer_son()
             except Exception as e:
                 print(f" Erreur : {e}")
+                await asyncio.sleep(20)
+
+    @discord.app_commands.command(name="sounds", description="Activer ou Désactiver les sons aléatoires")
+    @discord.app_commands.describe(etat="Choisis d'activer ou de désactiver les sons aléatoires")
+    @discord.app_commands.choices(etat=[
+        discord.app_commands.Choice(name="Activer", value="on"),
+        discord.app_commands.Choice(name="Désactiver", value="off")
+    ])
+    async def sounds(self, interaction: discord.Interaction, etat: str):
+        self.sounds_enabled = (etat == "on")
+
+        if not self.sounds_enabled:
+            guild = self.bot.get_guild(GUILD_ID)
+            if guild:
+                voice_client = discord.utils.get(self.bot.voice_clients, guild=guild)
+                if voice_client and voice_client.is_connected():
+                    voice_client.stop()
+                    await voice_client.disconnected()
+
+        emoji = "🎶" if self.sounds_enabled else "🔇"
+        await interaction.response.send_message(f"{emoji} - Sons Vocaux Aléatoires {"activé" if self.sounds_enabled else "désactivé"}")
+
+
 
 async def setup(bot):
-    await bot.add_cog(Sounds(bot))
+    print("Setup sound appelé")
+    cog=Sounds(bot)
+    await bot.add_cog(cog)
+    bot.loop.create_task(cog.boucle_aleatoire())
